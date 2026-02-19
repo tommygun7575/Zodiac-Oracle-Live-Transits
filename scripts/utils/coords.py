3c323f571b242088b912b6fc73e2b348dc5a9227
IamMyOwnMaster <tommygun7575@gmail.com>
2026-02-18 17:34:02 -0800

    Add docstring and TODOs to coords.py
    Add docstring for coordinate transformation utilities with TODOs.

--- scripts/utils/coords.py
+++ scripts/utils/coords.py
@@ -1,1 +1,8 @@
 
+"""
+Coordinate Transformation Utilities
+
+TODO:
+    - Implement ecliptic <-> RA/DEC conversions using math utils.
+    - Provide accurate astronomical coordinate conversions for all ephemeris engines.
+"""
