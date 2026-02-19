2c05a2d87c43aed2f672e3fcf986424987683c03
IamMyOwnMaster <tommygun7575@gmail.com>
2026-02-18 17:37:40 -0800

    Add docstring and TODOs to time_utils.py
    Add a docstring with TODOs for time utility functions.

--- scripts/utils/time_utils.py
+++ scripts/utils/time_utils.py
@@ -1,1 +1,7 @@
+"""
+Time Utility Functions
 
+TODO:
+    - Implement UTC parsing, ISO8601 conversions, and timezone normalization.
+    - Support universal time calculations for live transit feeds.
+"""
