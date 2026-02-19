e4e1eb20ac68fa67f0d6cdff9374b6dbc97c16d1
IamMyOwnMaster <tommygun7575@gmail.com>
2026-02-18 20:09:56 -0800

    Rename coords.py to corrd.py

---
+++ scripts/utils/corrd.py
@@ -0,0 +1,7 @@
+"""
+Coordinate Transformation Utilities
+
+TODO:
+    - Implement ecliptic <-> RA/DEC conversions using math utils.
+    - Provide accurate astronomical coordinate conversions for all ephemeris engines.
+"""
