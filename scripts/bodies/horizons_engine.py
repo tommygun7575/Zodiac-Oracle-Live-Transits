fce0a45cd9b019bf4bed5913f1e63bbdb0027ba3
IamMyOwnMaster <tommygun7575@gmail.com>
2026-03-07 06:44:45 -0800

    Rename get_body and get_numbered_object functions

--- scripts/bodies/horizons_engine.py
+++ scripts/bodies/horizons_engine.py
@@ -1,9 +1,17 @@
 from astroquery.jplhorizons import Horizons
 import math
 
-def get_body(body, jd):
+
+def fetch(body, jd):
+
     try:
-        obj = Horizons(id=body, location="500@10", epochs=jd)
+
+        obj = Horizons(
+            id=body,
+            location="500@10",
+            epochs=jd
+        )
+
         vec = obj.vectors()
 
         x = float(vec["x"][0])
@@ -14,12 +22,22 @@ def get_body(body, jd):
         lat = math.degrees(math.atan2(z, (x*x+y*y)**0.5))
 
         return lon, lat, "horizons"
+
     except Exception:
+
         return None
 
-def get_numbered_object(number, jd):
+
+def fetch_numbered(number, jd):
+
     try:
-        obj = Horizons(id=f"{number};", location="500@10", epochs=jd)
+
+        obj = Horizons(
+            id=f"{number};",
+            location="500@10",
+            epochs=jd
+        )
+
         vec = obj.vectors()
 
         x = float(vec["x"][0])
@@ -30,5 +48,7 @@ def get_numbered_object(number, jd):
         lat = math.degrees(math.atan2(z, (x*x+y*y)**0.5))
 
         return lon, lat, "horizons"
+
     except Exception:
+
         return None
