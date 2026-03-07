61e88a5f121b6e581ea67473eb7b36c160b404bf
IamMyOwnMaster <tommygun7575@gmail.com>
2026-03-07 07:28:19 -0800

    Add compute_harmonics function to calculate harmonics

--- scripts/bodies/harmonics_engine.py
+++ scripts/bodies/harmonics_engine.py
@@ -1,1 +1,13 @@
 
+import numpy as np
+
+
+def compute_harmonics(longitudes):
+
+    lons = np.array(longitudes)
+
+    return {
+        "h5": (lons * 5) % 360,
+        "h7": (lons * 7) % 360,
+        "h9": (lons * 9) % 360
+    }
