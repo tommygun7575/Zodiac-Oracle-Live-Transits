2e3c99a2ed8f2219a5946282b2b24bdec82e4e24
IamMyOwnMaster <tommygun7575@gmail.com>
2026-03-08 17:01:08 -0700

    Add targets for celestial bodies in targets.py

---
+++ scripts/targets.py
@@ -0,0 +1,38 @@
+TARGETS = {
+
+    # Major planets
+    "Sun": "10",
+    "Moon": "301",
+    "Mercury": "199",
+    "Venus": "299",
+    "Mars": "499",
+    "Jupiter": "599",
+    "Saturn": "699",
+    "Uranus": "799",
+    "Neptune": "899",
+    "Pluto": "999",
+
+    # Major dwarfs / TNOs
+    "Eris": "136199",
+    "Haumea": "136108",
+    "Makemake": "136472",
+    "Sedna": "90377",
+    "Quaoar": "50000",
+    "Orcus": "90482",
+
+    # Centaurs
+    "Chiron": "2060",
+    "Chariklo": "10199",
+    "Pholus": "5145",
+
+    # Major asteroids (semicolon required)
+    "Ceres": "1;",
+    "Pallas": "2;",
+    "Juno": "3;",
+    "Vesta": "4;",
+
+    # Named
+    "Psyche": "16;",
+    "Eros": "433;",
+    "Amor": "1221;"
+}
